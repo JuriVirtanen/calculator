e28e18d00f08ea43602fc3d6b6e04e633a8ed5e3
Gabriel Marczak <121840299+JuriVirtanen@users.noreply.github.com>
2023-02-13 00:50:03 +0100

    Finish mvp

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import requests
 import csv
 from flask import Flask
-from flask import render_template
+from flask import render_template, redirect, request
 
 response = requests.get("http://api.nbp.pl/api/exchangerates/tables/C?format=json")
 data = response.json()
@@ -14,10 +14,21 @@ with open('kursy.csv', 'w', newline='') as plik:
 
 kursy = []
 for waluta in data:
-    kursy.append({"nazwa": waluta['currency'], "kurs": waluta['ask']})
+    kursy.append({"nazwa": waluta['code'], "kurs": waluta['ask']})
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html", kursy = kursy)
+
+
+@app.route("/calc", methods=["POST"])
+def calc():
+    oblicz = request.form
+    kodw = oblicz.get('kody')
+    ilew = oblicz.get('ile')
+    for sss in kursy:
+        if sss['nazwa'] == kodw:
+            koszt = float(sss['kurs'])*int(ilew)
+            return f"<p>Koszt zakupu wynosi {round(koszt, 2)} PLN</p>"
